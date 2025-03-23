86953f8311a96d0528bb439ce980bbcc348a19f6
DiegoSalazar20 <dsalazarzamora2001@gmail.com>
2025-03-23 10:04:04 -0600

    Mostrar publicidades en el espacio de publicidad con animacion

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit,AfterViewChecked, ViewChild, ElementRef,  } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ViewChild, ElementRef, } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-menu',
-  imports: [CommonModule, HttpClientModule ],
-  
+  imports: [CommonModule, HttpClientModule],
+
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
@@ -15,46 +15,46 @@ export class MenuComponent implements OnInit {
   constructor(private router: Router, private http: HttpClient) { }
 
   publicidades: any[] = [];
-  indiceActual = 0;
-  apiPublicidad='http://arenaymar.somee.com/api/Publicidad/Visibles'
-  animacionPendiente = false;
+  apiPublicidad = 'http://arenaymar.somee.com/api/Publicidad/Visibles'
   imagenAnterior: any = null;
+  listaPublicidades: any[] = [];
+  publicidadAnterior: any = null;
+  indiceActual: number = 0;
+  animacionPendiente: boolean = false;
 
   @ViewChild('imagen') imagen!: ElementRef<HTMLImageElement>;
 
 
   ngOnInit(): void {
-    this.http
-      .get<any[]>(this.apiPublicidad)
-      .subscribe((data) => {
-        this.publicidades = data;
-      });
+    this.http.get<any[]>(this.apiPublicidad).subscribe((datos) => {
+      this.listaPublicidades = datos;
+    });
   }
 
-  siguientePublicidad(): void {
-    if (this.publicidades.length > 0) {
-      this.imagenAnterior = this.publicidades[this.indiceActual];
-      this.indiceActual = (this.indiceActual + 1) % this.publicidades.length;
+  mostrarSiguientePublicidad(): void {
+    if (this.listaPublicidades.length > 0) {
+      this.publicidadAnterior = this.listaPublicidades[this.indiceActual];
+      this.indiceActual = (this.indiceActual + 1) % this.listaPublicidades.length;
       this.animacionPendiente = true;
       setTimeout(() => {
-        this.imagenAnterior = null;
+        this.publicidadAnterior = null;
       }, 400);
     }
   }
-  
+
 
   ngAfterViewChecked(): void {
     if (this.animacionPendiente) {
       const img = this.imagen?.nativeElement;
       if (img) {
-        img.classList.remove('slide-in');
+        img.classList.remove('entrar-derecha');
         void img.offsetWidth;
-        img.classList.add('slide-in');
+        img.classList.add('entrar-derecha');
         this.animacionPendiente = false;
       }
     }
   }
-  
+
 
   redirigir(ruta: string) {
     this.router.navigate([ruta]);
