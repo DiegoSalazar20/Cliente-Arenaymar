ef6c1a2f07fceaafb101a5cf5df0012098e4cb81
RitchelS <sojoritchel@gmail.com>
2025-03-31 11:16:34 -0600

    Modulación de métodos en la página sobre nosotros

--- src/app/sobrenosotros/sobrenosotros.component.ts
+++ src/app/sobrenosotros/sobrenosotros.component.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { MenuComponent } from '../menu/menu.component';
 
 @Component({
-  selector: 'app-facilidades',
+  selector: 'app-sobrenosotros',
   standalone: true,
   imports: [CommonModule, HttpClientModule, MenuComponent],
   templateUrl: './sobrenosotros.component.html',
@@ -14,32 +14,38 @@ export class SobrenosotrosComponent implements OnInit {
   facilidades: any[] = [];
   seleccionada: any = null;
   errorCargando: boolean = false;
-  textoSobreNosotros: any = null;
+  textoSobreNosotros: string = '';
 
   private apiUrl = 'http://arenaymar.somee.com/api/Facilidades/Visibles';
-  private apiUrlSobreNosotros =  'http://arenaymar.somee.com/api/SobreNosotros';
+  private apiUrlSobreNosotros = 'http://arenaymar.somee.com/api/SobreNosotros';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.cargarFacilidades();
+    this.cargarTextoSobreNosotros();
+  }
+
+  cargarFacilidades() {
     this.http.get<any[]>(this.apiUrl).subscribe({
       next: (data) => {
         this.facilidades = data;
-        this.seleccionada = data[0]; 
-
-        this.http.get<any[]>(this.apiUrlSobreNosotros).subscribe({
-          next: (data2) => {
-            this.textoSobreNosotros = data2;
-          },
-          error: (err2) => {
-            console.error('Error al cargar el texto descriptivo Sobre Nosotros:', err2);
-            this.errorCargando = true;
-          }
-        });  
-
+        this.seleccionada = data[0];
       },
       error: (err) => {
         console.error('Error al cargar facilidades:', err);
+        this.errorCargando = true;
+      }
+    });
+  }
+
+  cargarTextoSobreNosotros() {
+    this.http.get<any>(this.apiUrlSobreNosotros).subscribe({
+      next: (data) => {
+        this.textoSobreNosotros = data.texto;
+      },
+      error: (err) => {
+        console.error('Error al cargar el texto descriptivo Sobre Nosotros:', err);
         this.errorCargando = true;
       }
     });
